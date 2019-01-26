05079894607edb3af5ba4347cb5714db0d7ba5f2
Mobile PC <tahri.e@gmail.com>
2019-01-26 11:37:04 -0500

    Enenemy Range and Targeting

--- Assets/Scripts/Enemies/BasicEnemy.cs
+++ Assets/Scripts/Enemies/BasicEnemy.cs
@@ -7,20 +7,51 @@
         public Rigidbody2D rgbd2d;
         public float flipTime;
         public float speed;
+        public float enemyAttackRange;
+
 
         private float timer;
         private bool right;
-
+        private bool facing;
+        private bool inRange;
+        
         protected override void Init()
         {
             timer = 0;
             right = false;
         }
 
+
+        protected override void Attack()
+        {
+
+            Collider2D[] hitColliders = Physics2D.OverlapCircle(this.transform.position, enemyAttackRange, 1 << LayerMask.NameToLayer("Player"));
+
+            for (int i = 0; i < hitColliders.Length; i++)
+            {
+               if( hitColliders[i].gameObject.tag == "Player")
+                {
+                    inRange = true;
+                }
+                else
+                {
+                    inRange = false;
+                }
+            }
+        }
+
         protected override void Run()
         {
-            if((timer += Time.deltaTime) > flipTime)
+            
+            if((timer += Time.deltaTime) > flipTime )
             {
+                if (this.PlayerInRange())
+                {
+                    Target();
+                    timer = 0;
+                    return;
+                }
+
                 timer = 0;
                 right = !right;
                 speed = -speed;
@@ -34,6 +65,7 @@
                     this.transform.rotation = Quaternion.Euler(0, 180, 0);
                     speed = speed * -Mathf.Sign(speed);
                 }
+                
             }
 
             rgbd2d.velocity = new Vector2(speed, rgbd2d.velocity.y);
--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -6,8 +6,14 @@
     {
         public int maxHealth;
         public GameObject deathItem;
-
+        public GameObject player;
         protected int health;
+
+        private bool inRange;
+        private Vector3 vectorToTarget;
+        private float angle;
+        private Quaternion q;
+   
 
         private void Start()
         {
@@ -17,9 +23,14 @@
 
         private void Update()
         {
-            Run();
         }
-
+        private void Target()
+        {
+            vectorToTarget = player.transform - transform.position;
+            angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+            q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+        }
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.tag == "PlayerAttack")
@@ -28,8 +39,13 @@
                 if (health <= 0)
                     Die();
             }
+            if (other.gameObject.tag == "Player")
+            {
+                //Melee
+            }
         }
 
+        protected abstract void Attack();
         protected abstract void Init();
         protected abstract void Run();
 
