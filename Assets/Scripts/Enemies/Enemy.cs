02d18751a5152fcef8f32ff731a068874de2a5eb
Mobile PC <tahri.e@gmail.com>
2019-01-26 14:10:43 -0500

    Updated Enemys

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,13 +1,23 @@
 ﻿namespace HomeTakeover.Enemies
 {
     using UnityEngine;
+    using HomeTakeover.Util;
+    using HomeTakeover.Character;
+    
 
     public abstract class Enemy : MonoBehaviour
     {
         public int maxHealth;
+        public float speed = 3f;
         public GameObject deathItem;
+        public bool facing = false;
 
         protected int health;
+
+        private Vector3 vectorToTarget;
+        private float angle;
+        private Quaternion q;
+   
 
         private void Start()
         {
@@ -18,8 +28,39 @@
         private void Update()
         {
             Run();
+            Attack();  
         }
 
+        /*
+        Rotates sprite to face player
+        */
+        public void Target()
+        {
+            vectorToTarget = PlayerController.instance.gameObject.transform.position - transform.position;
+            angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+            q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+        }
+
+        /*
+        Method to confirm if enemy is facing player withing 10 deg angel
+        */
+        public void IsFacing()
+        {
+            float angle = 10;
+            if (Vector3.Angle(PlayerController.instance.gameObject.transform.forward, transform.position - PlayerController.instance.gameObject.transform.position) < angle)
+            {
+                facing = true;
+            }
+            else
+            {
+                facing = false;
+            }
+        }
+
+        /*
+       Collider behavoirs for Player and PlayerAttack
+        */
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.tag == "PlayerAttack")
@@ -28,11 +69,22 @@
                 if (health <= 0)
                     Die();
             }
+            if (collision.gameObject.tag == "Player")
+            {
+                //Melee
+            }
         }
 
+        /*
+        Abstract Methods
+        */
+        protected abstract void Attack();
         protected abstract void Init();
         protected abstract void Run();
 
+        /*
+        Method for handling death
+        */
         protected void Die()
         {
             GameObject item = Instantiate(deathItem);
