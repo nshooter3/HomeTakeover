5f1e7a29feb9cf12d063c9eb6de3f45134945002
Mitch McClellan <mmcclellan7@gatech.edu>
2019-01-26 13:35:56 -0500

    Sprite importer

---
+++ Assets/Scripts/Editor/ImportSprites.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeTakeover.Editor
+{
+    
+    using UnityEngine;
+    using UnityEditor;
+
+    public class ImportSprites : AssetPostprocessor
+    {
+        void OnPreprocessTexture()
+        {
+            if (assetPath.Contains("png"))
+            {   
+                TextureImporter textureImporter  = (TextureImporter)assetImporter;
+                textureImporter.spritePixelsPerUnit = 32;
+
+                textureImporter.maxTextureSize = 512;
+                textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+            }
+        }
+    }
+}
