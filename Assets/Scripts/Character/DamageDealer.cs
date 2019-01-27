1a0ea2d56ffb09abd4de4bc0fcdf9481adeb1b2a
nshooter3 <nick.shooter@gmail.com>
2019-01-26 20:50:16 -0500

    added damage dealer script, unique attack id to player attacks

---
+++ Assets/Scripts/Character/DamageDealer.cs
@@ -0,0 +1,22 @@
+﻿namespace HomeTakeover.Character
+{
+    using UnityEngine;
+
+    public class DamageDealer : MonoBehaviour
+    {
+        /// <summary>
+        /// Damage dealt to enemies
+        /// </summary>
+        public int damage;
+
+        /// <summary>
+        /// 0 stuns nothing, 1 stuns small enemies, 2 stuns big enemies
+        /// </summary>
+        public int stun;
+
+        /// <summary>
+        /// custom id to ensure that enemies don't take same attack twice
+        /// </summary>
+        public int attackId;
+    }
+}
--- Assets/Scripts/Furniture/Box.cs
+++ Assets/Scripts/Furniture/Box.cs
@@ -3,12 +3,57 @@
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using HomeTakeover.Character;
 
     public class Box : Furniture
     {
+        private float cooldown = 0, maxCooldown = 0.15f;
+
+        public float attackReach;
+        Vector3 attackPos;
+        Vector3 initScale;
+        Vector3 initPos;
+        SpriteRenderer sprite;
+
+        public DamageDealer punchDamage;
+
+        public ParticleSystem punchParticles;
+
+        private void Start()
+        {
+            sprite = GetComponentInChildren<SpriteRenderer>();
+            attackPos = new Vector3(0, -1 * attackReach, 0);
+            initScale = sprite.transform.localScale;
+            initPos = sprite.transform.localPosition;
+        }
+
         public override void OnUse()
         {
-            Debug.Log("BOX ATTACK!");
+            if (cooldown == 0)
+            {
+                cooldown = maxCooldown;
+                PlayerController.instance.MeleeWeaponPunch(maxCooldown);
+                punchParticles.Play();
+                ToggleHurtBox(true);
+                punchDamage.attackId = PlayerController.instance.RequestAttackId();
+                //print("ATTACK ID: " + punchDamage.attackId);
+            }
+        }
+
+        private void Update()
+        {
+            if (cooldown > 0)
+            {
+                cooldown = Mathf.Max(0, cooldown - Time.deltaTime);
+                float t = cooldown / maxCooldown;
+                sprite.transform.localPosition = Vector2.Lerp(initPos, initPos + attackPos, t);
+                sprite.transform.localScale = Vector3.Lerp(initScale, initScale * 1.5f, t);
+                sprite.color = Color.Lerp(Color.white, Color.red, t);
+                if (cooldown == 0)
+                {
+                    ToggleHurtBox(false);
+                }
+            }
         }
     }
 }
