4afa0918a623bf51070d8aff8f632a8eb122030e
Mitch McClellan <mitchelljmcclellan@gmail.com>
2019-01-27 09:45:06 -0500

    Sound (#18)
    * Sound player
    
    * Sounds fixed

---
+++ Assets/Scripts/Util/AudioTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HomeTakeover.Util;
+
+public class AudioTester : MonoBehaviour
+{
+
+    // public SoundPool.SoundTypes type;
+    [Range(0, 19)]
+    public int soundIndex;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            SoundPool.Instance.PlaySound(soundIndex);
+        }
+    }
+}
