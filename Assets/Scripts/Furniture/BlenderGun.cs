1939fb04fe885fdf4333b010843ad1136c868b70
nshooter3 <nick.shooter@gmail.com>
2019-01-27 13:10:53 -0500

    Blender gun pickup, todo: make it shoot (#24)

---
+++ Assets/Scripts/Furniture/BlenderGun.cs
@@ -0,0 +1,33 @@
+﻿namespace HomeTakeover.Furniture
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using HomeTakeover.Character;
+
+    public class BlenderGun : Furniture
+    {
+        public ParticleSystem gunParticles;
+
+        private SpriteRenderer sprite;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            sprite = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public override void OnUse()
+        {
+            gunParticles.Play();
+            int attackId = PlayerController.instance.RequestAttackId();
+        }
+    }
+}
--- Assets/Scripts/Furniture/FurniturePool.cs
+++ Assets/Scripts/Furniture/FurniturePool.cs
@@ -10,7 +10,7 @@
         [SerializeField]
         private int[] furniturePoolSizes;
 
-        public enum FurnitureTypes { Box }
+        public enum FurnitureTypes { Box , Blender}
 
         /// <summary> Singleton instance for this object pool. </summary>
         public static FurniturePool Instance { get; private set; }
